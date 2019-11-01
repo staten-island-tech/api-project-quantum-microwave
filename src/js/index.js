b7eba67d99ea040e1877528301178c9a6f3441f7
Rob Grande <robert.grande@sitechhs.com>
2019-11-01 14:02:24 -0400

    THIS WONT WORK 404 ERROR

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,23 @@
-const AvengerStrings = {
+/* const AvengerStrings = {
     input: document.getElementById()
 }
-
+ */
 //figure out which divs are to be changed
+/* 
+(async function() {
+    var response = await fetch(`http://gateway.marvel.com/v1/public/comics?title=spiderman&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`)
+    var response = await response.json()
+    console.log(response)
+})() */
 
-//http:gateway.marvel.com/v1/public/comics?ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=dae71ebd702838855c2ef3a7d803bf7b
+/* console.log(fetch(`http://gateway.marvel.com/v1/public/comics?title=spiderman&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`)); */
+
+async function test(){
+    const result = await fetch( //await will return a promise
+        `http://gateway.marvel.com/v1/public/comics&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+        );
+        const data = await result.json();
+        console.log(data);
+}
+
+test();
