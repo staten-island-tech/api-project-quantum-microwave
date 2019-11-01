d27210a8e9b7a218a3aff5f4bbcd7654ebb062ee
Rob Grande <robert.grande@sitechhs.com>
2019-11-01 11:48:13 -0400

    hash

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,5 @@ const AvengerStrings = {
 }
 
 //figure out which divs are to be changed
+
+//http:gateway.marvel.com/v1/public/comics?ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=dae71ebd702838855c2ef3a7d803bf7b
