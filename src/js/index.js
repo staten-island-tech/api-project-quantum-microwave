a36ca09ce97a67611930b4ec1d0978ccaef7c53b
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2019-11-01 11:48:25 -0400

    Update index.js

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+/* var api = require('marvel-api');
+ 
+var marvel = api.createClient({
+  publicKey: 'my-public-key'
+, privateKey: 'my-private-key'
+});
+
+marvel.characters.findAll(function(err, results) {
+    if (err) {
+      return console.error(err);
+    }
+   
+    console.log(results);
+  }); */
+
+ const privateKey = "7fa106a200d24cb0f9d190c0e2b4bb72";
+ const publicKey = "dae71ebd702838855c2ef3a7d803bf7b";
+ 
