c67d763f23b53ec95bee322d8a11980d57cb1d63
Rob Grande <robert.grande@sitechhs.com>
2019-11-04 11:23:58 -0500

    API CALL WORKS

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+async function test(){
+    const result = await fetch( //await will return a promise
+        `http://gateway.marvel.com/v1/public/comics?title=spider-man&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+        );
+        const data = await result.json();
+        console.log(data.data);
+}
+test();
