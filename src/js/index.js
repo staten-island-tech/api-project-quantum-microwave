c14c7de08aafcab13881139cb06afb421c1df06c
Rob Grande <robert.grande@sitechhs.com>
2019-11-04 11:47:05 -0500

    starting more work

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@
 
 async function test(){
     const result = await fetch( //await will return a promise
-        `http://gateway.marvel.com/v1/public/comics?title=spider-man&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+        `http://gateway.marvel.com/v1/public/characters?name=captain%20america&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
         );
         const data = await result.json();
         console.log(data.data);
