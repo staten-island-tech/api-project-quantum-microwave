218b43df4af67ef3a9ceee8a3dbe0b2a7659bbd9
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2019-11-06 18:09:30 -0500

    Marvel Folder API Works
    In the src folder in the Marvel folder

---
+++ src/marvel/app.js
@@ -0,0 +1,43 @@
+const DOMStrings = {
+  input: document.getElementById("pkmn-form"),
+  name: document.getElementById("marvel-name"),
+  displayName: document.querySelector(".mrvl-name-size"),
+  displayImage: document.querySelector(".display-thumbnail"),
+  displayDescrip: document.querySelector(".mrvl-descrip"),
+  type: document.querySelector(".type")
+};
+
+DOMStrings.input.addEventListener("keypress", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".nes-btn").click();
+  }
+});
+
+function getPkmn() {
+  DOMStrings.input.addEventListener("submit", async function(e) {
+    e.preventDefault();
+    try {
+        const result = await fetch(
+            `https://gateway.marvel.com:443/v1/public/characters?name=${DOMStrings.name.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+            );
+            const data = await result.json();
+            console.log(data);
+         
+
+
+      const displayPkmn = function(data) {
+        DOMStrings.displayName.innerText = data.data.results[0].name;
+        DOMStrings.displayDescrip.innerText = data.data.results[0].description;
+        DOMStrings.displayImage.src = data.data.results[0].thumbnail.path +'/detail' + "." + data.data.results[0].thumbnail.extension;
+        DOMStrings.type.textContent = data.data.types.map(data => data.data.type.name);
+      };
+
+      displayPkmn(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+}
+
+getPkmn();
