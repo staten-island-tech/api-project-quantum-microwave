ce3b9e42e8d5d3bc4a84d986477631ee6656d343
Rob Grande <robert.grande@sitechhs.com>
2019-11-07 11:17:28 -0500

    getting api calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,25 @@
 
 const DOMStrings = {
-    input: document.getElementById("avenger-form"),
-    name: document.getElementById("marvel-name"),
-    displayName: document.querySelector(".mrvl-name-size"),
-    displayImage: document.querySelector(".display-thumbnail"),
-    displayDescrip: document.querySelector(".mrvl-descrip"),
-    type: document.querySelector(".type")
-};
-
-DOMStrings.input.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.querySelector(".searchTerm").click();
-    }
-});
-
-function getAvenger() {
-    DOMStrings.input.addEventListener("submit", async function (e) {
-        e.preventDefault();
-        try {
-            const result = await fetch(
-                `https://gateway.marvel.com:443/v1/public/characters?name=${DOMStrings.name.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
-            );
-            const data = await result.json();
-            console.log(data);
-
-            const displayAvenger = function (data) {
-                DOMStrings.displayName.innerText = data.data.results[0].name;
-                DOMStrings.displayDescrip.innerText = data.data.results[0].description;
-                DOMStrings.displayImage.src = data.data.results[0].thumbnail.path + '/detail' + "." + data.data.results[0].thumbnail.extension;
-                DOMStrings.type.textContent = data.data.types.map(data => data.data.type.name);
-            };
-
-            displayAvenger(data);
-        } catch (err) {
-            console.log(err);
-        }
-    });
+    input: document.getElementById('avenger-form'), //comma because its an object
+    inputName: document.getElementById('text-area'),
+    displayName: document.querySelector('.display-name'),
+    displayThumbnail: document.querySelector('.display-thumbnail')
 }
 
-getAvenger();
+function getData(){
+    DOMStrings.input.addEventListener('submit', async function(e){
+        e.preventDefault();
+        const result = await fetch( //await will return a promise
+            `https://gateway.marvel.com:443/v1/public/characters?name=${DOMStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+            );
+            const data = await result.json();
+            console.log(data.data)
+
+            const displayThing = function(data){
+                DOMStrings.displayName.innerText = data.data.results[0].description;
+                DOMStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
+            };
+            displayThing(data);
+    })
+}
+getData();
