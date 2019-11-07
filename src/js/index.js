64565f18d9727ea0dbb6db32e72e1bb6e2a940f7
Rob Grande <robert.grande@sitechhs.com>
2019-11-07 11:27:12 -0500

    trying to get api

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,24 @@
 
-const DOMStrings = {
+const AvengerStrings = {
     input: document.getElementById('avenger-form'), //comma because its an object
-    inputName: document.getElementById('text-area'),
-    displayName: document.querySelector('.display-name'),
+    inputName: document.getElementById('search-term'),
+    displayName: document.querySelector('.marvel-name'),
+    displayDescrip: document.querySelector('.mrvl-descrip'),
     displayThumbnail: document.querySelector('.display-thumbnail')
 }
 
 function getData(){
-    DOMStrings.input.addEventListener('submit', async function(e){
+    AvengerStrings.input.addEventListener('submit', async function(e){
         e.preventDefault();
         const result = await fetch( //await will return a promise
-            `https://gateway.marvel.com:443/v1/public/characters?name=${DOMStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+            `https://gateway.marvel.com:443/v1/public/characters?name=${AvengerStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
             );
             const data = await result.json();
             console.log(data.data)
 
             const displayThing = function(data){
-                DOMStrings.displayName.innerText = data.data.results[0].description;
-                DOMStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
+                AvengerStrings.displayName.innerText = data.data.results[0].description;
+                AvengerStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
             };
             displayThing(data);
     })
