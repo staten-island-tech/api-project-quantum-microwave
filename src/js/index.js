6f1647fb3325b9999bb18a8a0be6fce9db58a406
Rob Grande <robert.grande@sitechhs.com>
2019-11-08 11:40:13 -0500

    JAVA ALL WORKS (need more images)

--- src/js/index.js
+++ src/js/index.js
@@ -6,27 +6,42 @@ const AvengerStrings = {
     displayDescription: document.querySelector('.marvel-description'),
     displayThumbnail: document.querySelector('.display-thumbnail')
 }
+AvengerStrings.input.addEventListener("keypress", function(e){
+    if (event.keyCode === 13){
+        e.preventDefault();
+        document.querySelector('.nes-btn').click();
+    }
+});
 
 function getData(){
     AvengerStrings.input.addEventListener('submit', async function(e){
         e.preventDefault();
-        const result = await fetch( //await will return a promise
-            `https://gateway.marvel.com:443/v1/public/characters?name=${AvengerStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
-            );
-            const data = await result.json();
-            console.log(data.data)
-
-            const displayThing = function(data){
-                AvengerStrings.displayName.innerText = data.data.results[0].name;
-                AvengerStrings.displayDescription.innerText = data.data.results[0].description;
-                AvengerStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
+        try{
+            const result = await fetch( //await will return a promise
+                `https://gateway.marvel.com:443/v1/public/characters?name=${AvengerStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+                );
+                const data = await result.json();
+                console.log(data.data)
+    
+                const displayThing = function(data){
+                    AvengerStrings.displayName.innerText = data.data.results[0].name;
+                    AvengerStrings.displayDescription.innerText = data.data.results[0].description;
+                    AvengerStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
+                };
+                displayThing(data);
+                AvengerStrings.inputName.value = '';
+        }catch(error){
+            console.log(error);
+            const invalid = function(){
+                AvengerStrings.displayName.innerText = 'Error: Marvel Character Does Not Exist!';
+                AvengerStrings.displayDescription.innerText = '';
+                AvengerStrings.displayThumbnail.src = '';
             };
-            displayThing(data);
+            invalid();
+            AvengerStrings.inputName.value = '';
+        }
     })
 }
 getData();
 
-//user inputs wrong thing, display error
-//clear field once thing is displayed
 //display more images
-//press enter and it displays
