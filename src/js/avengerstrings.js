cc324210d3a6718e77af79344b3e726f7b3a28bd
Rob Grande <robert.grande@sitechhs.com>
2019-11-12 11:28:22 -0500

    adding webpack and comics

---
+++ src/js/avengerstrings.js
@@ -0,0 +1,12 @@
+
+
+export const AvengerStrings = {
+    input: document.getElementById('avenger-form'), //comma because its an object
+    inputName: document.getElementById('search-term'),
+    displayName: document.querySelector('.marvel-name'),
+    displayDescription: document.querySelector('.marvel-description'),
+    displayThumbnail: document.querySelector('.display-thumbnail'),
+    displayImageOne: document.querySelector('.display-image-1'),
+    displayImageTwo: document.querySelector('.display-image-2'),
+    displayImageThree: document.querySelector('.display-image-3')
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,5 @@
+import {AvengerStrings} from './avengerstrings.js'
 
-const AvengerStrings = {
-    input: document.getElementById('avenger-form'), //comma because its an object
-    inputName: document.getElementById('search-term'),
-    displayName: document.querySelector('.marvel-name'),
-    displayDescription: document.querySelector('.marvel-description'),
-    displayThumbnail: document.querySelector('.display-thumbnail')
-}
 AvengerStrings.input.addEventListener("keypress", function(e){
     if (event.keyCode === 13){
         e.preventDefault();
@@ -22,14 +16,25 @@ function getData(){
                 );
                 const data = await result.json();
                 //console.log(data.data)
+
+            const result_comics = await fetch(
+                `https://gateway.marvel.com:443/v1/public/comics?title=${AvengerStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+                );
+                 const data_comics = await result_comics.json();
+                 //console.log(data_comics.data);
     
                 const displayThing = function(data){
                     AvengerStrings.displayName.innerText = data.data.results[0].name;
                     AvengerStrings.displayDescription.innerText = data.data.results[0].description;
                     AvengerStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
+                    AvengerStrings.displayImageOne.src = data_comics.data.results[2].images[0].path + '/standard_amazing.jpg';
+                    AvengerStrings.displayImageTwo.src = data_comics.data.results[3].images[0].path + '/standard_amazing.jpg';
+                    AvengerStrings.displayImageThree.src = data_comics.data.results[4].images[0].path + '/standard_amazing.jpg';
                 };
                 displayThing(data);
                 AvengerStrings.inputName.value = '';
+
+
         }catch(error){
             console.log(error);
             const invalid = function(){
@@ -43,5 +48,3 @@ function getData(){
     })
 }
 getData();
-
-//display more images
