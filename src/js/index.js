ecafc676eed49894be1a66599ec8c7e5647c8262
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2019-11-11 22:50:46 -0500

    lol

--- src/js/index.js
+++ src/js/index.js
@@ -4,22 +4,37 @@ const AvengerStrings = {
     inputName: document.getElementById('search-term'),
     displayName: document.querySelector('.marvel-name'),
     displayDescription: document.querySelector('.marvel-description'),
-    displayThumbnail: document.querySelector('.display-thumbnail')
+    displayThumbnail: document.querySelector('.display-thumbnail'),
+    displayImage1: document.querySelector('.display-image-1'),
+    displayImage2: document.querySelector('.display-image-2'),
+    displayImage3: document.querySelector('.display-image-3')
 }
 
 function getData(){
     AvengerStrings.input.addEventListener('submit', async function(e){
         e.preventDefault();
-        const result = await fetch( //await will return a promise
-            `https://gateway.marvel.com:443/v1/public/characters?name=${AvengerStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
-            );
-            const data = await result.json();
-            console.log(data.data)
-
-            const displayThing = function(data){
-                AvengerStrings.displayName.innerText = data.data.results[0].name;
-                AvengerStrings.displayDescription.innerText = data.data.results[0].description;
-                AvengerStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
+        try{
+            const result = await fetch( //await will return a promise
+                `https://gateway.marvel.com:443/v1/public/characters?name=${AvengerStrings.inputName.value}&ts=1&apikey=08bdc0969adbf9a79f5e6592b6980b85&hash=7eaf0892e7f5433321e3642146ee8928`
+                );
+                const data = await result.json();
+                console.log(data.data)
+    
+                const displayThing = function(data){
+                    AvengerStrings.displayName.innerText = data.data.results[0].name;
+                    AvengerStrings.displayDescription.innerText = data.data.results[0].description;
+                    AvengerStrings.displayThumbnail.src = data.data.results[0].thumbnail.path + '/standard_amazing.jpg';
+                    //AvengerStrings.displayImage1.src = data.data.results[0].images[0];
+                    //AvengerStrings.displayImage1.src = data.data.results[0].comics.id.thumbnail.path + '/standard_amazing.jpg';
+                };
+                displayThing(data);
+                AvengerStrings.inputName.value = '';
+        }catch(error){
+            console.log(error);
+            const invalid = function(){
+                AvengerStrings.displayName.innerText = 'Error: Marvel Character Does Not Exist!';
+                AvengerStrings.displayDescription.innerText = '';
+                AvengerStrings.displayThumbnail.src = '';
             };
             displayThing(data);
     })
